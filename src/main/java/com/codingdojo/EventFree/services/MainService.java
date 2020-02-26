b4d2110f45bd5cde341d7eb12424b4954c13ab1b
Risa Moreno <Risa.Moreno@gmail.com>
2020-02-26 13:22:56 -0800

    Added login/reg to Controller, added create/update/check if creator/delete methods in Service.

--- src/main/java/com/codingdojo/EventFree/services/MainService.java
+++ src/main/java/com/codingdojo/EventFree/services/MainService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.mindrot.jbcrypt.BCrypt;
 
+import com.codingdojo.EventFree.models.Event;
 import com.codingdojo.EventFree.models.User;
 import com.codingdojo.EventFree.repositories.EventRepository;
 import com.codingdojo.EventFree.repositories.UserRepository;
@@ -43,5 +44,43 @@ public class MainService {
     	} else {
     	    return null;
     	}
+   }    
+   public boolean authenticateUser(String email, String password) {
+       User user = userRepository.findByEmail(email);
+       if(user == null) {
+           return false;
+       } else {
+           if(BCrypt.checkpw(password, user.getPassword())) {
+               return true;
+           } else {
+               return false;
+           }
+       }
+   }
+   public Event createEvent(Event event) {
+   	return eventRepository.save(event);
+   }
+   public Event findEvent(Long id) {
+   	Event event = eventRepository.findById(id).orElse(null);
+   	if(event!=null) {
+   		return event;
+   	}
+   	return null;
+   }
+   public User findEventCreator(Long id) {
+   	Event event = eventRepository.findById(id).orElse(null);
+   	if(event!=null) {
+   		return event.getCreator();
+   	}
+   	return null;
+   }
+   public Event updateEvent(Event event) {
+   	if(event!=null) {
+   		return eventRepository.save(event);
+   	}
+   	return null;
+   }
+   public void deleteEvent(Long id) {
+   	eventRepository.deleteById(id);
    }
 }
