8eaf23ac6bc16940a536fda44cfe17a974807eb3
haben <haben317@gmail.com>
2020-02-26 10:02:15 -0800

    initial commit

---
+++ src/main/java/com/codingdojo/EventFree/EventFreeApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.EventFree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventFreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventFreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/EventFree/EventFreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.EventFree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventFreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
